a40affea2f9ace232a0dba8dcd687bdcccaa9a5f
张春鹏 <zhang.chunpeng@xcar.com.cn>
2016-05-27 10:45:25 +0800

    忽略Pod文件

--- Pods/Target Support Files/Pods-Swift2.0Study/Pods-Swift2.0Study-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Swift2_0Study : NSObject
-@end
-@implementation PodsDummy_Pods_Swift2_0Study
-@end
--- Pods/Target Support Files/Pods-Swift2.0Study/Pods-Swift2.0Study-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Swift2_0StudyVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Swift2_0StudyVersionString[];
-
